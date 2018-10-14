fa7baaaccfb254243f27a6d19e684d8445d73fb8
nimbus117 <mail@nimbus117.co.uk>
2018-10-14 18:24:25 +0100

    added Airport class and tests

--- src/main/java/Airport.java
+++ src/main/java/Airport.java
@@ -4,5 +4,40 @@ import java.util.ArrayList;
 
 public class Airport {
     private ArrayList<Plane> hanger;
+    private ArrayList<Flight> flights;
     private CodeType code;
+
+    public Airport(CodeType code) {
+        this.code = code;
+        hanger = new ArrayList<>();
+        flights = new ArrayList<>();
+    }
+
+    public CodeType getCode() {
+        return  code;
+    }
+
+    public void addPlaneToHanger(Plane plane) {
+        hanger.add(plane);
+    }
+
+    public int getHangerCount() {
+        return hanger.size();
+    }
+
+   public Flight createFlight(int number, CodeType destination) {
+       Flight flight = new Flight(number, destination);
+       flights.add(flight);
+       return flight;
+   }
+
+   public void assignPlaneToFlight(Plane plane, Flight flight) {
+        if (hanger.remove(plane)) {
+            flight.setPlane(plane);
+        }
+   }
+
+   public void addPassengerToFlight(Passenger passenger, Flight flight) {
+       flight.addPassenger(passenger);
+   }
 }
--- src/test/java/AirportTest.java
+++ src/test/java/AirportTest.java
@@ -1,2 +1,51 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertNull;
+
 public class AirportTest {
+   Plane plane1;
+   Airport airport;
+
+   @Before
+   public void before() {
+      plane1 = new Plane(PlaneType.BOEING737, AirlineType.BA);
+      airport = new Airport(CodeType.EDI);
+   }
+
+   @Test
+   public void canAddPlaneToHanger() {
+      airport.addPlaneToHanger(plane1);
+      assertEquals(1, airport.getHangerCount());
+   }
+
+   @Test
+   public void canCreateFlight() {
+      Flight flight = airport.createFlight(321, CodeType.MCT);
+      assertEquals(321, flight.getNumber());
+   }
+
+   @Test
+   public void canAssignPlaneToFlight() {
+      Flight flight = airport.createFlight(321, CodeType.MCT);
+      airport.addPlaneToHanger(plane1);
+      airport.assignPlaneToFlight(plane1, flight);
+      assertEquals(plane1, flight.getPlane());
+   }
+
+   @Test
+   public void cannotAssignPlaneToFlight() {
+      Flight flight = airport.createFlight(321, CodeType.MCT);
+      airport.assignPlaneToFlight(plane1, flight);
+      assertNull(flight.getPlane());
+   }
+
+   @Test
+   public void canAddPassengerToFlight() {
+      Flight flight = airport.createFlight(321, CodeType.MCT);
+      Passenger passenger = new Passenger("Bob");
+      airport.addPassengerToFlight(passenger, flight);
+      assertEquals(1, flight.getPassengerCount());
+   }
 }
